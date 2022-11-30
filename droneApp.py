1df124f0a5691b5a363a3258f05f4eb514fe3392
fxa76 <francois_albouy@epam.com>
2022-11-30 16:06:13 +0100

    test usb video input instead of simulation video input

--- droneApp.py
+++ droneApp.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QApplication
 from views import StartWindow
 from move_land import Navigator, TextLogger
 from video_from_drone import VideoCapture
+#from video_from_usb_camera import VideoCapture
 from landingTargetDetector import LandingTargetDetector
 from msgobj.cancellationToken import CancellationToken
 
@@ -18,11 +19,11 @@ if __name__ == '__main__':
     textLogger = TextLogger(message_stack)
     capture = VideoCapture(source_images_stack)
     landingTargetDetector = LandingTargetDetector(source_images_stack, landing_target_data, analyzed_img_stack)
-    navigator = Navigator(landing_target_data,textLogger)
+    navigator = Navigator(landing_target_data, textLogger)
     navigator.start()
 
     app = QApplication([])
-    start_window = StartWindow(analyzed_img_stack,message_stack,navigator,landing_target_data)
+    start_window = StartWindow(analyzed_img_stack, message_stack, navigator, landing_target_data)
     start_window.show()
     app.exit(app.exec_())
     continue_flag.cancel()
