e5651143797389e49be33f1f2b8675a2312b53f5
fxa76 <francois_albouy@epam.com>
2022-11-30 16:03:44 +0100

    add usb source for video

---
+++ video_from_usb_camera.py
@@ -0,0 +1,48 @@
+import airsim
+import threading
+import numpy as np
+import cv2
+import time
+
+from msgobj.cancellationToken import CancellationToken
+
+
+class VideoCapture:
+
+    def __init__(self,stack ):
+        self.continue_flag = CancellationToken()
+        self.stack = stack
+        print("waiting for camera")
+        self.cap = cv2.VideoCapture(0)
+        print("camera ready")
+        # print(self.client.simGetCameraInfo(str('down')))
+        thread = threading.Thread(target=self.start, )
+        thread.start()
+
+    def __del__(self):
+        print("video from drone stopped")
+        self.cap.release()
+
+    def start(self):
+        CAMERA_NAME = 'down'  # 'high_res'
+        IMAGE_TYPE = airsim.ImageType.Scene
+
+        while not self.continue_flag.is_cancelled:
+            time.sleep(0.01)
+            ret, frame = self.cap.read()
+            if frame is not None:
+                self.stack.append(frame)
+
+
+
+if __name__ == '__main__':
+    import time
+
+    source_image_stack = []
+    continue_flag = CancellationToken()
+    capture = VideoCapture(source_image_stack)
+    time_len= 10
+    time.sleep(time_len)
+    print("number of images stored after {} sec: {}".format(time_len, len(source_image_stack)))
+    continue_flag.cancel()
+    print("continue set to False")
